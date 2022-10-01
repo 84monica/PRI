4f619fcefd4794f90c4217bfd51f135057bea4e6
84monica <up201905753@fe.up.pt>
2022-10-01 18:00:45 +0100

    Organize Repo

--- plots.py
+++ plots.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-dataset = pd.read_csv('google_books_1299.csv')
+dataset = pd.read_csv('datasets/google_books_1299.csv')
 
 # PLOT AUTHORS WITH MOST BOOKS PUBLISHED
 # create serie to plot
@@ -21,6 +21,6 @@ authors_count.plot.bar()
 
 fig = matplotlib.pyplot.gcf()
 fig.set_size_inches(18.5, 10.5)
-fig.savefig('authors_count.png')
+fig.savefig('images/authors_count.png')
 plt.clf()
 
--- preparation.py
+++ preparation.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-data1299 = pd.read_csv('google_books_1299.csv', sep=',')
-dataset = pd.read_csv('google_books_dataset.csv', sep=',')
+data1299 = pd.read_csv('datasets/google_books_1299.csv', sep=',')
+dataset = pd.read_csv('datasets/google_books_dataset.csv', sep=',')
 
 dataset = dataset.rename(columns={ 'categories' : 'generes', 'pageCount' : 'page_count', 'averageRating' : 'rating', 'publishedDate' : 'published_data'})
 data1299 = data1299.rename(columns={ 'author' : 'authors'})
