316a64628ebd58105c0516c820dfea691f059861
84monica <up201905753@fe.up.pt>
2022-10-01 18:34:37 +0100

    Add Ratings Plot

--- plots.py
+++ plots.py
@@ -1,15 +1,14 @@
+from copy import copy
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
 dataset = pd.read_csv('datasets/google_books_1299.csv')
+print(dataset)
 
 # PLOT AUTHORS WITH MOST BOOKS PUBLISHED
-# create serie to plot
 authors_count = dataset.groupby(dataset["author"]).count()["ISBN"].sort_values(ascending=False).head(10)
-
-print(authors_count)
 
 # configure plot
 plt.rcParams.update({"font.size": 8})
@@ -24,3 +23,21 @@ fig.set_size_inches(18.5, 10.5)
 fig.savefig('images/authors_count.png')
 plt.clf()
 
+# RATINGS
+tmp_dataset = copy(dataset)
+tmp_dataset["rating"] = round(tmp_dataset["rating"])
+
+ratings_count = tmp_dataset.groupby(tmp_dataset["rating"]).count()["ISBN"]
+
+# configure plot
+plt.rcParams.update({"font.size": 8})
+plt.ylabel("Number of Books")
+plt.title("Ratings")
+
+# create plot
+ratings_count.plot.bar()
+
+fig = matplotlib.pyplot.gcf()
+fig.set_size_inches(18.5, 10.5)
+fig.savefig('images/ratings.png')
+plt.clf()
