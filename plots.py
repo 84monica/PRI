1ad86b948247187d76fcd90db4dd3db253ad225b
84monica <up201905753@fe.up.pt>
2022-10-01 17:57:23 +0100

    Add plots.py

---
+++ plots.py
@@ -0,0 +1,26 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+dataset = pd.read_csv('google_books_1299.csv')
+
+# PLOT AUTHORS WITH MOST BOOKS PUBLISHED
+# create serie to plot
+authors_count = dataset.groupby(dataset["author"]).count()["ISBN"].sort_values(ascending=False).head(10)
+
+print(authors_count)
+
+# configure plot
+plt.rcParams.update({"font.size": 8})
+plt.ylabel("Books Published")
+plt.title("Authors with the most Books Published")
+
+# create plot
+authors_count.plot.bar()
+
+fig = matplotlib.pyplot.gcf()
+fig.set_size_inches(18.5, 10.5)
+fig.savefig('authors_count.png')
+plt.clf()
+
