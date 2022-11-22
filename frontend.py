8e98b1b3329e5480e2c69c3714ca489161e9b59a
84monica <up201905753@fe.up.pt>
2022-11-22 12:20:46 +0000

    Add Python Script base code for user interface : reading data from terminal and executing query

---
+++ frontend.py
@@ -0,0 +1,18 @@
+import json
+from unicodedata import category
+import requests
+
+# SEARCH
+search_url = 'http://localhost:8983/solr/books/select'
+
+title = input("Insert title: ")
+category = input("Insert category: ")
+
+query = {
+    'q': 'title:'+title+'AND categories:'+category,
+}
+
+response = requests.post(search_url, query)
+
+for doc in json.loads(response.text)["response"]["docs"]:
+    print("> %s" % (doc['title']))
